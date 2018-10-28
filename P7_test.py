163449f298a3de6a58d93379c9921ab3abfbc4a7
gov-sanap <sanapgovind1@gmail.com>
2018-10-28 20:24:58 +0530

    Add files via upload

---
+++ P7_test.py
@@ -0,0 +1,34 @@
+import numpy as np
+w = np.genfromtxt('w.csv', delimiter=',')
+v= np.genfromtxt('v.csv', delimiter=',')
+z=np.array([[0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0], [0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0], [0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0], [0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0], [0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0], [0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0], [0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0], [0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0], [0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0], [0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0]])
+
+def f(v,z,l,t):
+    net=np.matmul(v,z)
+    if t==1:
+        return 1 / (1 +np.exp(-net*l))
+    else:
+        return (1 - np.exp(-net*l)) / (1 +np.exp(-net*l))
+def fd(o,t):
+    if t==1:
+        return o*(1-o)
+    else:
+        return (1-o**2)
+hln=10#int(input("Enter the number of neurons in hidden layer\n"))
+oln=10
+n=len(z)#no.of input samples
+zd=len(z[0])#each sample dimension after augmentaion
+t=0#int(input("Activation Fuction as unipolar sigmoid for 1 or bipolar sigmoid for 0\n"))
+l=1#denotes value of lambda
+c=0.1 #denotes value of learning rate
+y=[]
+o=[]
+for i in range(0,n):
+    ys=[]
+    ys=[f(v[j,:],z[i,:],l,t) for j in range(0,hln)]
+    ys.append(-1)
+    y.append(ys)
+    os=[]
+    os=[f(w[j,:],ys,l,t) for j in range(0,oln)]
+    print("Output for "+str(i+1)+"th pattern is "+str(os))
+
